9216cc8af34a7c0aa3df55339afd28ef1e63cb30
arkteck <arkteck@gmail.com>
2025-08-18 12:54:57 -0400

    commit 15

---
+++ foundation/web/response.go
@@ -0,0 +1,33 @@
+package web
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+// Respond converts a Go value to JSON and sends it to the client.
+func Respond(ctx context.Context, w http.ResponseWriter, data any, statusCode int) error {
+	// setStatusCode(ctx, statusCode)
+
+	if statusCode == http.StatusNoContent {
+		w.WriteHeader(statusCode)
+		return nil
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		// return fmt.Errorf("web.respond: marshal: %w", err)
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if _, err := w.Write(jsonData); err != nil {
+		// return fmt.Errorf("web.respond: write: %w", err)
+		return err
+	}
+
+	return nil
+}
