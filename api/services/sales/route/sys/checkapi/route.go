739af1809183f6532e4b781c1364668925bb84d6
arkteck <arkteck@gmail.com>
2025-08-24 12:24:23 -0400

    commit 19

--- api/services/sales/route/sys/checkapi/route.go
+++ api/services/sales/route/sys/checkapi/route.go
@@ -7,5 +7,6 @@ func Routes(app *web.App) {
 	app.HandleFunc("GET /liveness", liveness)
 	app.HandleFunc("GET /readiness", readiness)
 	app.HandleFunc("GET /testerror", testError)
+	app.HandleFunc("GET /testpanic", testPanic)
 
 }
